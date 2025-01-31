bbdba058df339e4d6d7b81e002f910c1f6121fe2
Jonyleo <joao.ferreira.oliveira@tecnico.ulisboa.pt>
2025-01-31 15:25:55 +0000

    PMRace comparsion script fix

--- scripts/exp_seed_seperate.sh
+++ scripts/exp_seed_seperate.sh
@@ -4,6 +4,19 @@ SCRIPT_PATH=/home/vagrant/scripts/debug_workload.sh
 OUTPUT_PATH=/home/vagrant/pm-workloads/FAST_FAIR/concurrent_pmdk/output/
 DOWNLOAD_PATH=/home/vagrant/download/pmrace_results
 
+KICKTHETIRES=$1
+
+
+if [ -z $KICKTHETIRES ] ; then
+	TIMEOUT=10m
+	SEED_MAX=1000
+else
+	TIMEOUT=30s
+	SEED_MAX=10
+
+	echo Running seed analysis for Kick-the-tires
+fi
+
 rm $DOWNLOAD_PATH/* -fr
 
 SEED_ID=0
@@ -17,12 +30,16 @@ for seed in seeds/full/ff/* ; do
 
 	mkdir $DOWNLOAD_PATH/seed$SEED_ID -p 
 
-	timeout 10m $SCRIPT_PATH fast_fair sample
+	timeout 10s $SCRIPT_PATH fast_fair sample
 
 	rm $OUTPUT_PATH/seed0/*/ -rf
 	cp $OUTPUT_PATH/seed0/* $DOWNLOAD_PATH/seed$SEED_ID
 	cp $seed $DOWNLOAD_PATH/seed$SEED_ID/
 
 	SEED_ID=$(expr $SEED_ID + 1)
+
+	if [ $SEED_ID -eq $SEED_MAX ] ; then
+		exit
+	fi
 done
 
